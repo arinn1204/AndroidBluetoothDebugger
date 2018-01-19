1ddfdd8bda408be514fc39be5bd971d9658e09d0
Aaron Rinn <arinn1250@gmail.com>
2018-01-18 23:57:30 -0800

    initial commit

---
+++ app/src/main/java/co/rinn/androidbluetoothdebugger/Transceiver.java
@@ -0,0 +1,13 @@
+package co.rinn.androidbluetoothdebugger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Transceiver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transceiver);
+    }
+}
